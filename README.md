9d05de26e92e22e0f48deddcb286da67636ba2c5
olalekan <lekanmojibola@gmail.com>
2023-12-30 00:39:25 +0100

    This is a python code

---
+++ README.md
@@ -0,0 +1,3 @@
+This folder contains the RSA factoring project.
+This folder is created by Mojibola Olalekan.
+Happy coding!
---
+++ factor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+def get_prime(files):
+    if not isinstance(files, str):
+        sys.exit(1)
+    with open(files, "r") as file:
+        for line in file:
+            n = int(line)
+            div_sor = 2
+            prime_no = []
+            while n > 1:
+                if n % div_sor == 0:
+                    prime_no.append(div_sor)
+                    prime_no.append(int(n // div_sor))
+                    break
+                else:
+                    div_sor += 1
+            print("{:d}={}*{}".format(n, prime_no[0], prime_no[1]))
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage ./{} filename".format(sys.argv[0]))
+    else:
+        files = sys.argv[1]
+    get_prime(files)
